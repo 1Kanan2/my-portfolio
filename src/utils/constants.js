436e667f6e4adac3b30e6217199ddb73c91af33a
Keyner H. <keynerivan@outlook.com>
2026-06-02 16:04:44 -0500

    feat: add spanish CV buttons and file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
-import cvFile from '../assets/docs/KEYNER_HANCCO-CV.pdf';
+import cvFile from '../assets/docs/Hancco Poma Keyner Ivan CV ES.pdf';
 import certificateSCRUM from '../assets/docs/CERT-SCRUM_FUNDAMENTALS.pdf';
 
 export const ASSETS = {
-    CV_PATH: cvFile, CV_NAME: 'KEYNER_HANCCO-CV.pdf'
+    CV_PATH: cvFile, CV_NAME: 'Hancco Poma Keyner Ivan CV ES.pdf'
 };
 
 export const NAV_LINKS = [{id: 'home', key: 'nav.home', href: '#home'}, {
